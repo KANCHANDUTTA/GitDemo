40445cf158cc6e1d34870be20b8c62fcd6853eeb
Kanchan <kanchan.dutta15@gmail.com>
2022-10-26 15:01:30 +0530

    first commit

---
+++ src/main/java/QAclickacademy/Mavenjava/SeleniumTest.java
@@ -0,0 +1,18 @@
+package QAclickacademy.Mavenjava;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+	@Test
+	public void BrowserAutomation()
+	{
+		System.out.println("BrowserAutomation");
+	}
+	
+	@Test
+	public void elementsUI()
+	{
+		System.out.println("ElementsUI");
+	}
+
+}
