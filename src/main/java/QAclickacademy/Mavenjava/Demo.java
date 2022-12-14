8f9fa31ff59f4b8efc0cb026a5ef15cf25c3740d
Kanchan <kanchan.dutta15@gmail.com>
2022-12-14 21:30:33 +0530

    one methos added

---
+++ src/main/java/QAclickacademy/Mavenjava/Demo.java
@@ -0,0 +1,18 @@
+package QAclickacademy.Mavenjava;
+
+import org.testng.annotations.Test;
+
+public class Demo {
+
+	
+		// TODO Auto-generated method stub
+		@Test
+		public void nativeAppAndroidss()
+		{
+			System.out.println("nativeAppAndroidd");
+			System.out.println("nativeAppAndroid");
+		}
+
+	
+
+}
